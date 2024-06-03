c3b4cd64bc455028d56c6666be7f28122f48e0ba
Cesar Briseño <142867500+CesarBr1397@users.noreply.github.com>
2024-06-03 00:08:54 -0600

    Add files via upload

---
+++ Calculadora_Main.java
@@ -0,0 +1,18 @@
+package calculadora_swing;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author brise
+ */
+public class Calculadora_Main {
+	public static void main(String args[]) {
+		Calculadora_swing ventana = new Calculadora_swing();
+		ventana.setLocationRelativeTo(null); //Para que aparezca centrado
+		ventana.setVisible(true);
+	}
+}
